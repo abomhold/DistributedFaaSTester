7aa6deaa1f4ada2f1bdfdf4029cad2629296ea9c
austin <austinbomhold@gmail.com>
2025-01-12 19:07:30 -0800

    working

--- trace/src/main/java/lambda/Main.java
+++ trace/src/main/java/lambda/Main.java
@@ -12,22 +12,27 @@ import java.util.Map;
 
 public class Main implements RequestHandler<HashMap<String, Object>, HashMap<String, Object>> {
 
-//    private static void parseBody(Map<String, Object> request) {
-//        try {
-//            new ObjectMapper()
-//                    .readValue(request.remove("body").toString(), new TypeReference<Map<String, String>>() {
-//                    })
-//                    .entrySet()
-//                    .stream()
-//                    .forEach(e -> request.put(e.getKey(), e.getValue()));
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    private static void parseBody(Map<String, Object> request) {
+        try {
+            new ObjectMapper()
+                    .readValue(request.remove("body").toString(), new TypeReference<Map<String, String>>() {
+                    })
+                    .entrySet()
+                    .stream()
+                    .forEach(e -> request.put(e.getKey(), e.getValue()));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public HashMap<String, Object> handleRequest(HashMap<String, Object> request, Context context) {
-
-//        context.getLogger().log("payload" + request.get("body").toString() + "initTime" + );
+        ObjectMapper om = new ObjectMapper();
+        Map<String,Object> eventlog = Map.of(
+                "payload", request.get("body").toString(),
+                "nodeId", context.getClientContext().getEnvironment()
+        );
+        om.writerWithDefaultPrettyPrinter().writeValue();
+        context.getLogger().log("payload" + request.get("body").toString() + "" + );
         return new HashMap<>(Map.of("requestId", context.getAwsRequestId()));
     }
 }
